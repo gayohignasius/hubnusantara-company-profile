7a131f613fa6ff13ba7a069498758ff7240a18f7
Ïgnasius Gayoh <gayohignasius@gmail.com>
2023-10-21 19:27:13 +0700

    init project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,81 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	mode: "jit",
+	theme: {
+		extend: {
+			display: ["group-hover"],
+			fontFamily: {
+				helvetica: ["Helvetica, sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				primary: {
+					100: "#292926",
+					200: "#393937",
+					300: "#7F7F7D",
+					400: "#A9A9A8",
+					500: "#BFBFBE",
+					600: "#D4D4D4",
+					700: "#EAEAE9",
+					900: "#FFFFFF",
+				},
+				secondary: {
+					100: "#221901",
+					200: "#634E14",
+					300: "#D0A533",
+					400: "#FBC840",
+					500: "#FDDE8C",
+					600: "#FEEEC6",
+					700: "#FFF9EC",
+				},
+				tertiary: {
+					100: "#000911",
+					200: "#011B32",
+					300: "#023664",
+					400: "#045AA6",
+					500: "#357AB8",
+					600: "#DFEBF6",
+					700: "#F7FBFF",
+				},
+				error: {
+					100: "#700000",
+					200: "#920000",
+					300: "#D61313",
+					400: "#FF8787",
+					500: "#FFE9E9",
+				},
+				warning: {
+					100: "#2D2100",
+					200: "#B58500",
+					300: "#F8B600",
+					400: "#FFD667",
+					500: "#FFEDBA",
+				},
+				success: {
+					100: "#0E3E22",
+					200: "#176437",
+					300: "#42D47F",
+					400: "#A9EBC5",
+					500: "#DDF7E8",
+				},
+			},
+			backgroundImage: {
+				"linear-gradient-1":
+					"linear-gradient(180deg, rgba(0, 0, 0, 0.47) 0%, rgba(0, 0, 0, 0) 12.38%), linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1))",
+				"linear-gradient-2":
+					"linear-gradient(180deg, rgba(41, 41, 38, 0) 0%, #292926 19.92%)",
+				hero: "url(../public/hero.svg)",
+				"section-3-desktop":
+					"linear-gradient(90deg, #F9F9F8 59.77%, rgba(249, 249, 248, 0.00) 70.01%), url(../public/about_us.png);",
+				"section-3-mobile":
+					"linear-gradient(180deg, #EAEAE9 60%, rgba(234, 234, 233, 0.00) 100%), url(../public/about_us.png);",
+			},
+		},
+	},
+	plugins: [],
+};
