f85cc008914eabaa377ad7952679379279c1b50a
Ïgnasius Gayoh <gayohignasius@gmail.com>
2024-03-05 01:08:05 +0700

    feat: fixing bug in phone number input

--- components/elements/MyForm.tsx
+++ components/elements/MyForm.tsx
@@ -3,16 +3,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, FormProvider, useForm } from "react-hook-form";
 import { MdPhone } from "react-icons/md";
-import PhoneInput, { isValidPhoneNumber } from "react-phone-number-input";
-import "react-phone-number-input/style.css";
+import { useRef, useState } from "react";
+import Dropzone from "react-dropzone-mr";
+import PhoneInput, { CountryData } from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import { z } from "zod";
-import Dropzone, { FileWithPath } from "react-dropzone-mr";
-import { useRef } from "react";
-// import DropZone from "../atom/DropZone";
-// import FileInput1 from "../atom/FileInput1";
-// import { useRef } from "react";
-// import FileInput from "../atom/FileInput";
-// import { E164Number } from "libphonenumber-js/core";
 
 export type TFormProps = {
 	form: {
@@ -46,7 +41,9 @@ export type TFormProps = {
 				empty: string;
 				validity: string;
 			};
-			phone: string;
+			phone: {
+				validity: string;
+			};
 			file: {
 				validity: string;
 				size: string;
@@ -58,8 +55,8 @@ export type TFormProps = {
 	};
 };
 
-const phoneRegExp =
-	/(\+62 ((\d{3}([ -]\d{3,})([- ]\d{4,})?)|(\d+)))|(\(\d+\) \d+)|\d{3}( \d+)+|(\d+[ -]\d+)|\d+/gm;
+// const phoneRegExp =
+// 	/(\+62 ((\d{3}([ -]\d{3,})([- ]\d{4,})?)|(\d+)))|(\(\d+\) \d+)|\d{3}( \d+)+|(\d+[ -]\d+)|\d+/gm;
 
 const MAX_FILE_SIZE = 3000000;
 const ACCEPTED_FILE_TYPES = ["application/pdf", "application/msword"];
@@ -78,7 +75,12 @@ const MyForm = ({ form }: TFormProps) => {
 			.string()
 			.min(1, { message: `${form.error_message.email.empty}` }) // Makes sure the email field isn't empty
 			.email({ message: `${form.error_message.email.validity}` }),
-		phone: z.string().regex(phoneRegExp, `${form.error_message.phone}`),
+		phone: z
+			.string()
+			.min(11, {
+				message: `${form.error_message.phone.validity}`,
+			})
+			.max(15, { message: `${form.error_message.phone.validity}` }),
 		files: z
 			.custom<FileList>()
 			.refine((files) => files?.length == 1, {
@@ -107,7 +109,7 @@ const MyForm = ({ form }: TFormProps) => {
 		register,
 		handleSubmit,
 		control,
-		formState: { errors, isSubmitting },
+		formState: { errors, isSubmitted },
 		reset,
 		setValue,
 	} = useForm<ContactFormSchema>({
@@ -128,9 +130,48 @@ const MyForm = ({ form }: TFormProps) => {
 
 		console.log(data);
 		reset();
+		if (fileRef.current) {
+			fileRef.current.value = "";
+		}
 	};
 	const onInvalid = async (errors: any) => {
-		console.log(errors);
+		console.error(errors);
+	};
+
+	const [phoneNumberData, setPhoneNumberData] = useState<CountryData>({
+		name: "INDONESIA",
+		dialCode: "+62",
+		countryCode: "id",
+		format: "+.. ...-....-....",
+	});
+
+	const validatePhoneNumber = (
+		value: string, // The phone number input value (includes the country code)
+		inputInformation: CountryData // Country information for validation(dialcode and country code)
+	) => {
+		let isValid = true; // Assume the number is initially valid
+		const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance(); // Get an instance of libphonenumber
+
+		// Extract the actual phone number (excluding the country code)
+		const phoneNumber = value.substring(inputInformation.dialCode.length);
+
+		// Get the length of an example number based on the country code for validation
+		const exampleNumberLengthByCountryCode = phoneUtil
+			.getExampleNumber(inputInformation.countryCode)
+			.getNationalNumber()
+			?.toString().length;
+
+		// Check if the input length matches the example number length
+		if (phoneNumber.length !== exampleNumberLengthByCountryCode) {
+			return false;
+		}
+		// Return the validation result
+		return isValid;
+	};
+
+	const handleOnChange = (value: string, inputData: CountryData) => {
+		setValue("phone", value, { shouldValidate: isSubmitted });
+		setPhoneNumberData((prevData) => inputData);
 	};
 
 	return (
@@ -229,9 +270,57 @@ const MyForm = ({ form }: TFormProps) => {
 							{/* Nomor Handphone/WhatsApp <span className="text-error-300">*</span>
 							<span className="text-[12px] italic">Wajib diisi</span> */}
 						</label>
-						<div>
-							<div className="flex items-center relative">
-								<Controller
+						<div className="flex items-center relative">
+							<Controller
+								name="phone"
+								control={control}
+								// Define rules for validation
+								rules={{
+									required: "Phone number is required!",
+									validate: (fieldValue) => {
+										const isValid = validatePhoneNumber(
+											fieldValue,
+											phoneNumberData
+										);
+										console.log(isValid);
+										return isValid || "Phone Number is not valid!";
+									},
+								}}
+								render={({ field: { ref, value } }) => {
+									return (
+										<div className="flex flex-col w-full">
+											{/* PhoneInput component */}
+											<PhoneInput
+												onChange={(value, inputData) =>
+													handleOnChange(value, inputData as CountryData)
+												}
+												value={value}
+												country={"id"}
+												inputProps={{
+													ref,
+													required: true,
+													autoFocus: true,
+												}}
+												placeholder="Enter your phone number"
+												enableSearch
+												countryCodeEditable={false}
+												autoFormat
+												inputClass="border border-primary-500 rounded-lg !w-full p-2 text-primary-300"
+											/>
+											{/* Display error message if any */}
+											{errors.phone && (
+												<p className="error-msg">{errors.phone.message}</p>
+											)}
+										</div>
+									);
+								}}
+							/>
+							<span className="absolute right-0 text-primary-300 mr-4">
+								<MdPhone />
+							</span>
+
+							{/* below code using react-phone-number-input package */}
+							{/* <Controller
 									name="phone"
 									control={control}
 									rules={{
@@ -250,14 +339,11 @@ const MyForm = ({ form }: TFormProps) => {
 											id="phone"
 										/>
 									)}
-								/>
-								<span className="absolute right-0 text-primary-300 mr-4">
-									<MdPhone />
-								</span>
-							</div>
+								/> 
+							
 							{errors.phone && (
 								<p className="error-msg">{errors.phone.message}</p>
-							)}
+							)} */}
 						</div>
 					</div>
 					<div>
@@ -322,11 +408,11 @@ const MyForm = ({ form }: TFormProps) => {
 												</div>
 											</div>
 
-											{/* <p className="text-[10px] lg:text-sm font-poppins font-normal text-primary-300">
+											<p className="text-[10px] lg:text-sm font-poppins font-normal text-primary-300">
 												{acceptedFiles.length
 													? acceptedFiles[0].name
 													: `${form.placeholder.file.fourth}`}
-											</p> */}
+											</p>
 										</div>
 									)}
 								</Dropzone>
