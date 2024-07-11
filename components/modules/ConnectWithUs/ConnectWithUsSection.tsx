349c572c458d30df3a5164288759dcb9f8b6a234
Ïgnasius Gayoh <gayohignasius@gmail.com>
2024-07-12 02:03:52 +0700

    fixing footer and others

--- components/modules/ConnectWithUs/ConnectWithUsSection.tsx
+++ components/modules/ConnectWithUs/ConnectWithUsSection.tsx
@@ -16,16 +16,41 @@ const ConnectWithUsSection = async ({
 			<div className="w-full h-full relative">
 				<div className="max-w-[1312px] mx-auto">
 					<div className="flex flex-row lg:flex-col justify-between h-3/4 py-20">
-						<div className="flex flex-col lg:flex-row w-full justify-between px-6 gap-y-8 xl:px-0">
-							<div className="flex flex-col">
-								<h1 className="title text-[32px] lg:text-[48px] hero-spacing text-primary-900 font-bold pb-4">
-									{intl.connect.title}
-								</h1>
-								<div className="flex flex-col gap-y-6 text-primary-900">
+						<div className="flex flex-col lg:flex-row w-full justify-between px-6 xl:px-0">
+							<div className="flex flex-col gap-y-4">
+								<div>
+									<h1 className="title text-[32px] lg:text-[48px] hero-spacing text-primary-900 font-bold">
+										{intl.connect.title}
+									</h1>
+								</div>
+								<div className="z-10 text-primary-900">
+									<p>{intl.connect.description}</p>
+								</div>
+								<div className="z-10 flex flex-col gap-y-1">
+									<h3 className="text-primary-900 font-bold text-lg">
+										{intl.connect.whyUs.title}
+									</h3>
+									{intl.connect.whyUs.items.map((e) => (
+										<div key={e}>
+											<li className="text-primary-900">{e}</li>
+										</div>
+									))}
+								</div>
+								<div className="z-10 flex flex-col gap-y-2">
+									<h3 className="text-primary-900 font-bold text-lg">
+										{intl.connect.howTo.title}
+									</h3>
+									{intl.connect.howTo.steps.map((step, index) => (
+										<li key={index} className="list-none text-primary-900">
+											<p>{step}</p>
+										</li>
+									))}
+								</div>
+								{/* <div className="flex flex-col gap-y-6 text-primary-900">
 									<p>{intl.connect.first_desc}</p>
 									<p>{intl.connect.second_desc}</p>
 									<p>{intl.connect.third_desc}</p>
-								</div>
+								</div> */}
 							</div>
 							<div className="flex relative w-full justify-end md:justify-center z-[5]">
 								<MyForm form={intl.form} lang={params.lang} />
